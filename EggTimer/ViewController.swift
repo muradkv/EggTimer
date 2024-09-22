64edc56730f8aba588a5c4574541240913a1c4ef
murad <muradkv@gmail.com>
2024-09-22 03:57:40 +0300

    Initial commit

---
+++ EggTimer/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by murad on 22.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+
+}
+
